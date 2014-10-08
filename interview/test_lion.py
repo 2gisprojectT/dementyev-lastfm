f3e9f5c43e613f155d85db4afc1e1f770417f7cd
devgen <takinyash@gmail.com>
2014-10-08 23:45:56 +0700

    Add raise test

--- interview/test_lion.py
+++ interview/test_lion.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from unittest import TestCase
+import unittest
 from interview import states
 from interview.lion import Lion
 
@@ -23,7 +24,7 @@ class TestLion(TestCase):
         self.assertEqual(lion.reaction, states.HungryState().possible_reaction['hunter'])
         self.assertEqual(lion.state.__class__, states.HungryState().consequence_state['hunter'])
 
-        #now lion is hungry and
+        # now lion is hungry and
         lion.meet('tree')
         self.assertEqual(lion.reaction, states.HungryState().possible_reaction['tree'])
         self.assertEqual(lion.state.__class__, states.HungryState().consequence_state['tree'])
@@ -43,9 +44,13 @@ class TestLion(TestCase):
         self.assertEqual(lion.reaction, states.HungryState().possible_reaction['antelope'])
         self.assertEqual(lion.state.__class__, states.HungryState().consequence_state['antelope'])
 
-        #now lion is fed and
+        # now lion is fed and
         lion.meet('tree')
         self.assertEqual(lion.reaction, states.FedState().possible_reaction['tree'])
         self.assertEqual(lion.state.__class__, states.FedState().consequence_state['tree'])
 
+    def test_raise(self):
+        lion = Lion()
 
+        # wrong input
+        self.assertRaises(Exception, lion.meet, 'bla')
